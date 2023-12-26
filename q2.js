1f0e070d5a70cc753686ccb7fd5a092dbcced221
ingy <ingy.mahmoud.elsakhawy@gmail.com>
2023-12-26 15:15:04 +0200

    Second Question Completion

---
+++ q2.js
@@ -0,0 +1,9 @@
+alert("Welcome to the First Release of the Simple Calculator!\nThis version supports summation only.");
+
+            
+            var num1 = prompt("Enter the first number:")
+            var num2 = prompt("Enter the second number:");
+            
+                var sum = Number(num1) + Number(num2);
+                alert("The sum of " + num1 + " and " + num2 + " is: " + sum);
+            
